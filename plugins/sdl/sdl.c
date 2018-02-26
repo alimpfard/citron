44bca2cdf028d7703c04d87ab38175ff01b7e92e
AnotherTest <devanothertest@gmail.com>
2018-02-26 22:56:48 +0330

    Add a bunch more stuff to the SDL module

--- plugins/sdl/sdl.c
+++ plugins/sdl/sdl.c
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <unistd.h>
 #include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_rotozoom.h>
 
 #define NEW 1
 
@@ -56,7 +58,7 @@ ctr_object* ctr_sdl_color_get(ctr_object* myself, ctr_argument* argumentList);
 /*
  * SDL Surface interface
  */
-ctr_object* ctr_sdl_surface_loadBMP(ctr_object* myself, ctr_argument* argumentList);
+ctr_object* ctr_sdl_surface_loadImage(ctr_object* myself, ctr_argument* argumentList);
 ctr_object* ctr_sdl_surface_free_surface(ctr_object* myself, ctr_argument* argumentList);
 ctr_object* ctr_sdl_surface_display_format(ctr_object* myself, ctr_argument* argumentList);
 ctr_object* ctr_sdl_surface_lock(ctr_object* myself, ctr_argument* argumentList);
@@ -65,6 +67,10 @@ ctr_object* ctr_sdl_surface_set_pixel(ctr_object* myself, ctr_argument* argument
 ctr_object* ctr_sdl_surface_blit(ctr_object* myself, ctr_argument* argumentList);
 ctr_object* ctr_sdl_surface_blit_loc(ctr_object* myself, ctr_argument* argumentList);
 ctr_object* ctr_sdl_surface_fill(ctr_object* myself, ctr_argument* argumentList);
+ctr_object* ctr_sdl_surface_get_clip_rect(ctr_object* myself, ctr_argument* argumentList);
+ctr_object* ctr_sdl_surface_scale(ctr_object* myself, ctr_argument* argumentList);
+ctr_object* ctr_sdl_surface_scale_s(ctr_object* myself, ctr_argument* argumentList);
+ctr_object* ctr_sdl_surface_set_color_key(ctr_object* myself, ctr_argument* argumentList);
 ctr_object* ctr_sdl_surface_update(ctr_object* myself, ctr_argument* argumentList);
 /*
  * SDL Event interface
@@ -116,6 +122,7 @@ ctr_sdl_evt* get_sdl_event_ptr(ctr_object* myself) {
 }
 
 void ctr_sdl_quit_atexit() {
+  IMG_Quit();
   SDL_Quit();
 }
 
@@ -172,9 +179,9 @@ ctr_object* ctr_sdl_init(ctr_object* myself, ctr_argument* argumentList) {
     ctr_build_string_from_cstring("windowHeight"),
     CTR_CATEGORY_PRIVATE_PROPERTY
   )->value.nvalue;
-  if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) {
     char* err = "Unable to initialize SDL: ";
-    strcat(err, SDL_GetError());
+    err = strcat(err, SDL_GetError());
     CtrStdFlow = ctr_build_string_from_cstring(err);
     ctr_heap_free(caption);
     return CtrStdFlow;
@@ -183,9 +190,18 @@ ctr_object* ctr_sdl_init(ctr_object* myself, ctr_argument* argumentList) {
   SDL_Surface* window = SDL_SetVideoMode(w, h, 0, 0);
   if (window == NULL) {
     char* err = "Unable to set Video Mode: ";
-    strcat(err, SDL_GetError());
+    err = strcat(err, SDL_GetError());
     CtrStdFlow = ctr_build_string_from_cstring(err);
     ctr_heap_free(caption);
+    return CtrStdFlow;
+  }
+
+  // load support for the JPG and PNG image formats
+  int flags=IMG_INIT_JPG|IMG_INIT_PNG;
+  int initted=IMG_Init(flags);
+  if((initted&flags) != flags) {
+    char* err = strcat("IMG failed to initialize: ", IMG_GetError());
+    CtrStdFlow = ctr_build_string_from_cstring(err);
     return CtrStdFlow;
   }
   ctr_heap_free(caption);
@@ -194,8 +210,18 @@ ctr_object* ctr_sdl_init(ctr_object* myself, ctr_argument* argumentList) {
   return myself;
 }
 ctr_object* ctr_sdl_quit(ctr_object* myself, ctr_argument* argumentList) {
+  IMG_Quit();
   SDL_Quit();
   return CtrStdNil;
+}
+
+SDL_Rect* ctr_sdl_rect_make_nat(int x, int y, int w, int h) {
+  SDL_Rect* Rect = ctr_heap_allocate (sizeof(SDL_Rect) );
+  Rect->x = x;
+  Rect->y = y;
+  Rect->w = w;
+  Rect->h = h;
+  return Rect;
 }
 
 ctr_object* ctr_sdl_rect_make(ctr_object* myself, ctr_argument* argumentList) {
@@ -204,11 +230,7 @@ ctr_object* ctr_sdl_rect_make(ctr_object* myself, ctr_argument* argumentList) {
   y = argumentList->next->object->value.nvalue;
   w = argumentList->next->next->object->value.nvalue;
   h = argumentList->next->next->next->object->value.nvalue;
-  SDL_Rect* Rect = ctr_heap_allocate_tracked (sizeof(SDL_Rect) ); //We're gonna keep this 'till the end.
-  Rect->x = x;
-  Rect->y = y;
-  Rect->w = w;
-  Rect->h = h;
+  SDL_Rect* Rect = ctr_sdl_rect_make_nat(x,y,w,h);
   ctr_object* instance = ctr_sdl_create_container_of_type(CTR_SDL_TYPE_RECT);
   instance->link = CtrStdSdl_rect;
   instance->value.rvalue->ptr = (void*)Rect;
@@ -248,10 +270,10 @@ ctr_object* ctr_sdl_rect_alterh(ctr_object* myself, ctr_argument* argumentList) 
    ctr_object* instance = ctr_internal_create_object(CTR_OBJECT_TYPE_OTOBJECT);
    instance->link = myself;
    if(argumentList == NULL) return instance;
-   ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("red"), (argumentList->object), CTR_CATEGORY_PRIVATE_PROPERTY);
-   ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("green"), (argumentList->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
+   ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("red"),  (argumentList->object), CTR_CATEGORY_PRIVATE_PROPERTY);
+   ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("green"),(argumentList->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
    ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("blue"), (argumentList->next->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
-   ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("alpha"), (argumentList->next->next->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
+   ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("alpha"),(argumentList->next->next->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
    return instance;
  }
 
@@ -273,7 +295,13 @@ ctr_object* ctr_sdl_rect_alterh(ctr_object* myself, ctr_argument* argumentList) 
    CtrStdFlow = ctr_build_string_from_cstring("Expected an array of 4 values for RGBA");
    return myself;
  }
-
+ ctr_object* ctr_sdl_color_set_rgb(ctr_object* myself, ctr_argument* argumentList) {
+   ctr_internal_object_set_property(myself, ctr_build_string_from_cstring("red"), (argumentList->object), CTR_CATEGORY_PRIVATE_PROPERTY);
+   ctr_internal_object_set_property(myself, ctr_build_string_from_cstring("green"), (argumentList->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
+   ctr_internal_object_set_property(myself, ctr_build_string_from_cstring("blue"), (argumentList->next->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
+   if(argumentList->next->next->next->object) ctr_internal_object_set_property(myself, ctr_build_string_from_cstring("alpha"), (argumentList->next->next->next->object), CTR_CATEGORY_PRIVATE_PROPERTY);
+   return myself;
+ }
  ctr_object* ctr_sdl_color_make_rgb(ctr_object* myself, ctr_argument* argumentList) {
    if(argumentList->object->info.type != CTR_OBJECT_TYPE_OTARRAY) goto err_not_arr;
    if(ctr_array_count(argumentList->object, NULL)->value.nvalue != 3) goto err_not_enough;
@@ -308,7 +336,7 @@ ctr_object* ctr_sdl_rect_alterh(ctr_object* myself, ctr_argument* argumentList) 
    if(myself == CtrStdColor) return ctr_build_string_from_cstring("RGBA[null,null,null,null]");
    char str[512];
 
-   int r,g,b,a;
+   uint8_t r,g,b,a;
    r = ctr_internal_object_find_property(myself, ctr_build_string_from_cstring("red"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
    g = ctr_internal_object_find_property(myself, ctr_build_string_from_cstring("green"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
    b = ctr_internal_object_find_property(myself, ctr_build_string_from_cstring("blue"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
@@ -325,7 +353,7 @@ ctr_object* ctr_sdl_rect_alterh(ctr_object* myself, ctr_argument* argumentList) 
  *  SDL surface handler functions
  */
 
-ctr_object* ctr_sdl_surface_loadBMP(ctr_object* myself, ctr_argument* argumentList) {
+ctr_object* ctr_sdl_surface_loadImage(ctr_object* myself, ctr_argument* argumentList) {
   ctr_object* instance = ctr_sdl_create_container_of_type(CTR_SDL_TYPE_SURFACE);
   instance->link = CtrStdSdl_surface;
   char* loc = ctr_heap_allocate_cstring (argumentList->object);
@@ -334,9 +362,11 @@ ctr_object* ctr_sdl_surface_loadBMP(ctr_object* myself, ctr_argument* argumentLi
     ctr_heap_free(loc);
     return CtrStdExit;
   }
-  SDL_Surface* temp = SDL_LoadBMP(loc);
-  temp = SDL_DisplayFormat(temp);
+  SDL_Surface* temp = IMG_Load(loc);
+  SDL_Surface* tempcompat = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
   ctr_heap_free(loc);
+  instance->value.rvalue->ptr = tempcompat;
   return instance;
 }
 
@@ -358,8 +388,13 @@ ctr_object* ctr_sdl_surface_display_format(ctr_object* myself, ctr_argument* arg
 ctr_object* ctr_sdl_surface_blit(ctr_object* myself, ctr_argument* argumentList) {
   SDL_Surface* on = get_sdl_surface_ptr(myself);
   SDL_Surface* what = get_sdl_surface_ptr(argumentList->object);
-  printf("blit\n");
-  SDL_BlitSurface(what, NULL, on, NULL);
+  // printf("blit\n");
+  if(SDL_BlitSurface(what, NULL, on, NULL) < 0) {
+    char* err;
+    err = strcat("Blit failed: ", SDL_GetError());
+    CtrStdFlow = ctr_build_string_from_cstring(err);
+    return myself;
+  }
   return myself;
 }
 
@@ -367,8 +402,8 @@ ctr_object* ctr_sdl_surface_blit_loc(ctr_object* myself, ctr_argument* argumentL
   SDL_Surface* on = get_sdl_surface_ptr(myself);
   SDL_Surface* what = get_sdl_surface_ptr(argumentList->object);
   SDL_Rect *from, *to;
-  from = get_sdl_rect_ptr(argumentList->next->object);
-  to = get_sdl_rect_ptr(argumentList->next->next->object);
+  from = argumentList->next->object == CtrStdNil ? NULL : get_sdl_rect_ptr(argumentList->next->object);
+  to = argumentList->next->next->object == CtrStdNil ? NULL : get_sdl_rect_ptr(argumentList->next->next->object);
   SDL_BlitSurface(what, from, on, to);
   return myself;
 }
@@ -379,7 +414,7 @@ ctr_object* ctr_sdl_surface_fill(ctr_object* myself, ctr_argument* argumentList)
   #ifdef NEW
   //TODO: Get color interface
   ctr_object* color_ = argumentList->next->object;
-  int r,g,b;
+  uint8_t r,g,b;
   r = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("red"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
   g = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("green"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
   b = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("blue"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
@@ -394,8 +429,64 @@ ctr_object* ctr_sdl_surface_fill(ctr_object* myself, ctr_argument* argumentList)
   return myself;
 }
 
+ctr_object* ctr_sdl_surface_get_clip_rect(ctr_object* myself, ctr_argument* argumentList) {
+  SDL_Rect* rect = ctr_heap_allocate(sizeof(SDL_Rect));
+  SDL_Surface* surface = get_sdl_surface_ptr(myself);
+  SDL_GetClipRect(surface, rect);
+  ctr_object* instance = ctr_sdl_create_container_of_type(CTR_SDL_TYPE_RECT);
+  instance->link = CtrStdSdl_rect;
+  instance->value.rvalue->ptr = (void*)rect;
+  return instance;
+}
+
+
+ctr_object* ctr_sdl_surface_scale(ctr_object* myself, ctr_argument* argumentList) {
+  SDL_Surface* surface = get_sdl_surface_ptr(myself);
+  double amount = ctr_internal_cast2number(argumentList->object)->value.nvalue;
+  myself->value.rvalue->ptr = zoomSurface(surface, amount, amount, 1);
+  SDL_FreeSurface(surface);
+  return myself;
+}
+ctr_object* ctr_sdl_surface_scale_s(ctr_object* myself, ctr_argument* argumentList) {
+  SDL_Surface* surface = get_sdl_surface_ptr(myself);
+  double w = ctr_internal_cast2number(argumentList->object)->value.nvalue;
+  double h = ctr_internal_cast2number(argumentList->next->object)->value.nvalue;
+  myself->value.rvalue->ptr = zoomSurface(surface, w/((double)surface->w), h/((double)surface->h), 1);
+  SDL_FreeSurface(surface);
+  return myself;
+}
+
+
+ctr_object* ctr_sdl_surface_set_color_key(ctr_object* myself, ctr_argument* argumentList) {
+  SDL_Surface* surface = get_sdl_surface_ptr(myself);
+  uint32_t flag = SDL_SRCCOLORKEY | SDL_RLEACCEL;
+  #ifdef NEW
+  uint32_t color = 0;
+  if(argumentList->next->object) {
+    flag = ctr_internal_cast2bool(argumentList->next->object)->value.nvalue ? SDL_SRCCOLORKEY : 0;
+  }
+  if(flag != 0) {
+    ctr_object* color_ = argumentList->object;
+    uint8_t r,g,b;
+    r = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("red"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
+    g = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("green"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
+    b = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("blue"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
+    color = SDL_MapRGB(surface->format, r, g, b);
+  }
+  #else
+  uint32_t color = ctr_internal_cast2number(argumentList->next->object)->value.nvalue;
+  #endif
+  if(SDL_SetColorKey(surface, flag, color) < 0) {
+    char* err = strcat("setColorKey failed: ", SDL_GetError());
+    CtrStdFlow = ctr_build_string_from_cstring(err);
+  }
+  return myself;
+}
+
+
 ctr_object* ctr_sdl_surface_update(ctr_object* myself, ctr_argument* argumentList) {
   SDL_Surface* on = get_sdl_surface_ptr(myself);
+  SDL_Flip(on);
   SDL_UpdateRect(on, 0, 0, 0, 0);
   return myself;
 }
@@ -442,19 +533,64 @@ void ctr_sdl_put_pixel(SDL_Surface *surface, int x, int y, uint32_t pixel)
         break;
     }
 }
+void ctr_sdl_get_pixel(SDL_Surface *surface, int x, int y, uint32_t* pixel)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to get */
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        *pixel = *p;
+        break;
+
+    case 2:
+        *pixel = *(Uint16 *)p;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            *pixel = p[0] << 16 | p[1] << 8 | p[2];
+        } else {
+          *pixel = p[0] | p[1] << 8 | p[2] << 16;
+        }
+        break;
+
+    case 4:
+        *pixel = *(Uint32 *)p;
+        break;
+    }
+}
 ctr_object* ctr_sdl_surface_set_pixel(ctr_object* myself, ctr_argument* argumentList) {
   SDL_Surface* surface = get_sdl_surface_ptr(myself);
   int x, y;
   x = argumentList->object->value.nvalue;
   y = argumentList->next->object->value.nvalue;
   ctr_object* color_ = argumentList->next->next->object;
-  int r,g,b;
+  uint8_t r,g,b;
   r = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("red"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
   g = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("green"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
   b = ctr_internal_object_find_property(color_, ctr_build_string_from_cstring("blue"), CTR_CATEGORY_PRIVATE_PROPERTY)->value.nvalue;
   uint32_t color = SDL_MapRGB(surface->format, r, g, b);
   ctr_sdl_put_pixel(surface, x, y, color);
   return myself;
+}
+ctr_object* ctr_sdl_surface_get_pixel(ctr_object* myself, ctr_argument* argumentList) {
+  SDL_Surface* surface = get_sdl_surface_ptr(myself);
+  int x, y;
+  x = argumentList->object->value.nvalue;
+  y = argumentList->next->object->value.nvalue;
+  uint32_t pixel;
+  ctr_sdl_get_pixel(surface, x, y, &pixel);
+  uint8_t r,g,b;
+  SDL_GetRGB(pixel, surface->format, &r, &g, &b);
+  ctr_object* instance = ctr_internal_create_object(CTR_OBJECT_TYPE_OTOBJECT);
+  instance->link = CtrStdColor;
+  ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("red"),   ctr_build_number_from_float(r), CTR_CATEGORY_PRIVATE_PROPERTY);
+  ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("green"), ctr_build_number_from_float(g), CTR_CATEGORY_PRIVATE_PROPERTY);
+  ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("blue"),  ctr_build_number_from_float(b), CTR_CATEGORY_PRIVATE_PROPERTY);
+  ctr_internal_object_set_property(instance, ctr_build_string_from_cstring("alpha"), ctr_build_number_from_float(0), CTR_CATEGORY_PRIVATE_PROPERTY);
+  return instance;
 }
 #endif
 /**
@@ -584,20 +720,29 @@ void begin() {
   CtrStdSdl_evt = ctr_internal_create_object(CTR_OBJECT_TYPE_OTOBJECT);
   CtrStdSdl_evt->link = CtrStdObject;
   CtrStdColor = ctr_internal_create_object(CTR_OBJECT_TYPE_OTOBJECT);
-  //sdl, surface
+  //sdl, surface, rect
 	ctr_internal_create_func(CtrStdSdl, ctr_build_string_from_cstring( "new" ), &ctr_sdl_make );
   ctr_internal_create_func(CtrStdSdl, ctr_build_string_from_cstring( "init" ), &ctr_sdl_init );
   ctr_internal_create_func(CtrStdSdl, ctr_build_string_from_cstring( "quit" ), &ctr_sdl_quit );
   ctr_internal_create_func(CtrStdSdl, ctr_build_string_from_cstring( "set:to:" ), &ctr_sdl_set_prop);
-  ctr_internal_create_func(CtrStdSdl, ctr_build_string_from_cstring( "loadBMP:" ), &ctr_sdl_surface_loadBMP );
+  ctr_internal_create_func(CtrStdSdl, ctr_build_string_from_cstring( "loadImage:" ), &ctr_sdl_surface_loadImage );
   ctr_internal_create_func(CtrStdSdl, ctr_build_string_from_cstring( "newRectWithX:andY:andW:andH:" ), &ctr_sdl_rect_make );
 
   ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "blit:" ), &ctr_sdl_surface_blit );
   ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "blit:fromRect:toRect:" ), &ctr_sdl_surface_blit_loc );
   ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "update" ), &ctr_sdl_surface_update );
   ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "setPixelWithX:andY:toColor:" ), &ctr_sdl_surface_set_pixel);
+  ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "pixelAtX:andY:" ), &ctr_sdl_surface_get_pixel);
   ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "fillRect:withColor:" ), &ctr_sdl_surface_fill );
+  ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "clipRect" ), &ctr_sdl_surface_get_clip_rect );
+  ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "scale:" ), &ctr_sdl_surface_scale );
+  ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "scaleToW:andH:" ), &ctr_sdl_surface_scale_s );
+  ctr_internal_create_func(CtrStdSdl_surface, ctr_build_string_from_cstring( "setColorKey:" ), &ctr_sdl_surface_set_color_key );
 
+  ctr_internal_create_func(CtrStdSdl_rect, ctr_build_string_from_cstring( "setX:"), &ctr_sdl_rect_alterx);
+  ctr_internal_create_func(CtrStdSdl_rect, ctr_build_string_from_cstring( "setY:"), &ctr_sdl_rect_altery);
+  ctr_internal_create_func(CtrStdSdl_rect, ctr_build_string_from_cstring( "setW:"), &ctr_sdl_rect_alterw);
+  ctr_internal_create_func(CtrStdSdl_rect, ctr_build_string_from_cstring( "setH:"), &ctr_sdl_rect_alterh);
   //event
   ctr_internal_create_func(CtrStdSdl_evt, ctr_build_string_from_cstring( "new" ), &ctr_sdl_event_make );
   ctr_internal_create_func(CtrStdSdl_evt, ctr_build_string_from_cstring( "poll:" ), &ctr_sdl_event_poll_event );
@@ -613,6 +758,8 @@ void begin() {
   ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "red:green:blue:alpha:" ), &ctr_sdl_color_make);
   ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "rgba:" ), &ctr_sdl_color_make_rgba);
   ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "rgb:" ), &ctr_sdl_color_make_rgb);
+  ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "setRed:green:blue:" ), &ctr_sdl_color_set_rgb);
+  ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "setRed:green:blue:alpha:" ), &ctr_sdl_color_set_rgb);
   ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "get:" ), &ctr_sdl_color_get);
   ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "set:to:" ), &ctr_sdl_color_set);
   ctr_internal_create_func(CtrStdColor, ctr_build_string_from_cstring( "toString" ), &ctr_sdl_color_to_string);

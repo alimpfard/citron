aa6cbf4df25445134aa5114a53e5e6792fb0e8f8
Ali Mohammad Pur Fard <ali.mpfard@gmail.com>
2018-03-30 11:23:00 +0430

    revert the plugin derp

--- makeplugs.sh
+++ makeplugs.sh
@@ -6,6 +6,9 @@ for plug in ${plugs[*]}
 do
     echo "building $plug";
     cd "plugins/$plug";
+    if [[ -e "./configure" ]]; then
+      ./configure
+    fi
     echo $(make >/dev/null 2>&1 && echo "Succ $plug" || echo "$plug Failed")
     cd ../..
 done

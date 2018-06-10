23e042e6e1c51205e34aced79e11be1dc6747c47
Ali Mohammad Pur Fard <ali.mpfard@gmail.com>
2018-06-10 20:47:56 +0430

    Fix importlib reporting the wrong search paths

--- importlib.c
+++ importlib.c
@@ -11,12 +11,12 @@ ctr_importlib_begin (ctr_object * myself, ctr_argument * argumentList)
   ctr_object *instance = ctr_internal_create_object (CTR_OBJECT_TYPE_OTOBJECT);
   ctr_set_link_all (instance, myself);
   CtrStdImportLib_SearchPaths = ctr_array_new (CtrStdArray, NULL);
+  const char *epath = ctr_file_stdext_path_raw ();
   ctr_invoke_variadic (CtrStdImportLib_SearchPaths, &ctr_array_push, 1, ctr_build_string_from_cstring ("."));
-  ctr_invoke_variadic (CtrStdImportLib_SearchPaths, &ctr_array_push, 1, ctr_build_string_from_cstring (CTR_STD_EXTENSION_PATH));
+  ctr_invoke_variadic (CtrStdImportLib_SearchPaths, &ctr_array_push, 1, ctr_build_string_from_cstring ((char*)epath));
   ctr_internal_object_add_property (instance,
 				    ctr_build_string_from_cstring ("searchPaths"), CtrStdImportLib_SearchPaths, CTR_CATEGORY_PRIVATE_PROPERTY);
   ctr_internal_object_add_property (CtrStdWorld, ctr_build_string_from_cstring ("import"), instance, 0);
-  const char *epath = ctr_file_stdext_path_raw ();
   static char path_[2048];
   size_t len = sprintf (path_, "%s/extensions/importlib.ctr", epath);
   ctr_file_include_here (ctr_invoke_variadic (CtrStdFile, &ctr_file_new, 1, ctr_build_string (path_, len)), NULL);	//get all definitions

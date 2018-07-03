bc3a30ca763c430faff4cfb048172d0cd4563a8c
AnotherTest <devanothertest@gmail.com>
2018-07-04 03:08:49 +0430

    Add pragma highlighting to the grammars

--- integration/pygments/CitronLexer/citronlexer/lexer.py
+++ integration/pygments/CitronLexer/citronlexer/lexer.py
@@ -14,34 +14,34 @@ class CitronLexer(RegexLexer):
     tokens = {
         'root' : [
             (u'\\b(var|my|const)\\b', bygroups(Keyword.Pseudo), 'main__1'),
-            (u'(\\.)', bygroups(Name), ('#pop', 'root')),
+            (u'(\\$[!,\'`](?!=\\s))', bygroups(String.Escape)),
+            (u'(\\$(?=\\())', bygroups(String.Escape)),
             (u'([\\[\\]\\(\\)\\^\\}])', bygroups(Name)),
             (u'(?:(\\s*)(\\bis\\b|\\:\\=|\\=\\>)(\\s*))', bygroups(Generic, Keyword.Reserved, Generic)),
             (u'(?:(\\?>))', bygroups(String.Regex), 'main__2'),
             (u'(0[xX][0-9a-fA-F]+|0[cC][0-7]|\\d+\\.\\d+|\\d+)', bygroups(Number)),
             (u'(\')', bygroups(String), 'main__3'),
-            (u'(?:(\\{)((?:\\:(?:[^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*))*))', bygroups(Name, Keyword.Pseudo), 'main__4'),
+            (u'(?:(\\\\)((?:\\:[^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*\\s*)+))', bygroups(Keyword.Reserved, Keyword.Pseudo), 'main__4'),
+            (u'(?:(\\{)((?:\\:(?:[^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*))*))', bygroups(Name, Keyword.Pseudo), 'main__5'),
+            (u'(?:(\\#:)(.*)$)', bygroups(Comment, Generic.Deleted)),
             (u'(\\#.*$)', bygroups(Comment)),
-            (u'((?:[^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*)\\:)', bygroups(Name.Decorator), 'main__5'),
-            (u'(\\\\)', bygroups(String.Escape), 'main__6'),
-            (u'(?:(\\s+)([^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r])(\\s+))', bygroups(Name, Name.Decorator, Name), 'main__7'),
-            (u'([^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*)', bygroups(Generic), 'main__8'),
+            (u'((?:[^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*)\\:)', bygroups(Name.Decorator), 'main__6'),
+            (u'(\\\\)', bygroups(String.Escape), 'main__7'),
+            (u'(?:(\\s+)([^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r])(\\s+))', bygroups(Name, Name.Decorator, Name), 'main__8'),
+            (u'([^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*)', bygroups(Generic), 'main__9'),
             ('(\n|\r|\r\n)', String),
             ('.', String),
         ],
         'main__1' : [
-            (u'([^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*)', bygroups(Name.Variable), '#pop'),
             ('(\n|\r|\r\n)', String),
             ('.', Name.Variable),
         ],
         'main__2' : [
-            (u'(<\\?)', bygroups(String.Regex), '#pop'),
             (u'(.)', bygroups(String.Regex)),
             ('(\n|\r|\r\n)', String),
             ('.', String),
         ],
         'main__3' : [
-            (u'((?!<\\\\)\')', bygroups(String), '#pop'),
             (u'(\\\\[^xu\\n\\r])', bygroups(String.Escape)),
             (u'(?:(\\\\[xuXU])([{]?)([0-9a-fA-F]+)([}]?))', bygroups(String.Escape, String.Escape, Number, String.Escape)),
             (u'(.)', bygroups(String)),
@@ -49,36 +49,27 @@ class CitronLexer(RegexLexer):
             ('.', String),
         ],
         'main__4' : [
-            (u'(\\s*)', bygroups(Name), '#pop'),
             ('(\n|\r|\r\n)', String),
-            ('.', Name),
+            ('.', Generic),
         ],
         'main__5' : [
-            (u'(\\s*)', bygroups(Name), '#pop'),
             ('(\n|\r|\r\n)', String),
             ('.', Name),
         ],
         'main__6' : [
-            (u'(?:([^:\\n\\r]\\w*)|(\\:(?:[^\\s\\d:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r][^\\s:.,\\[\\]\\(\\)\\{\\}\\#\\n\\r]*)))', bygroups(String.Escape, Keyword.Reserved), '#pop'),
-            ('(\n|\r|\r\n)', String),
-            ('.', Keyword.Reserved),
-        ],
-        'main__7' : [
-            (u'(\\s*)', bygroups(Name), '#pop'),
             ('(\n|\r|\r\n)', String),
             ('.', Name),
         ],
+        'main__7' : [
+            ('(\n|\r|\r\n)', String),
+            ('.', Keyword.Reserved),
+        ],
         'main__8' : [
-            (u'(\\s*)', bygroups(Name), '#pop'),
+            ('(\n|\r|\r\n)', String),
+            ('.', Name),
+        ],
+        'main__9' : [
             ('(\n|\r|\r\n)', String),
             ('.', Name),
         ]
     }
-
-    def analyse_text(text):
-        shbang = re.match(r'\#\!.*ctr', text.strip().split('\n', 1)[0]) is not None
-        if shbang: return 1
-        maybe = len(filter(lambda x: x in text,
-            ['Reflect', 'Map', 'String', '?>', '<?',
-             'Pen', 'Library/', 'is']))
-        return maybe / 8

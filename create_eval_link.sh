8c6a287fd4b5cc7dfac8d6d34bf368fe1fb7dc62
Ali Mohammad Pur Fard <ali.mpfard@gmail.com>
2018-08-09 20:39:18 +0430

    Un-termuxify create_eval_link :P

--- create_eval_link.sh
+++ create_eval_link.sh
@@ -5,4 +5,5 @@ echo 'cwd=$(pwd)' >> run.sh
 printf 'cd %s && ./ctr eval --assume-non-tty --without-signals --without-history --terminal-width 40 "%s"\n' "`pwd`" '$@' >> run.sh
 echo 'cd $cwd' >> run.sh
 chmod a+x run.sh
-mv run.sh /data/data/com.termux/files/usr/bin/citron
+# ;P craaaazy
+mv run.sh $(dirname `which sh`)/citron 

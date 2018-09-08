1c0298deaff68153adbb920721709422d3e24620
Ali Mohammad Pur <ali.mpfard@gmail.com>
2018-09-08 06:14:33 +0430

    [build]

--- build_windows_package.bat
+++ build_windows_package.bat
@@ -10,7 +10,7 @@ echo MSYS2 system: %MSYSTEM%
 if %COMPILER%==msys2 (
     @echo on
     SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
-    pacman -S gcc
+    pacman -S --noconfirm gcc
     SET "CC=gcc.exe"
     gcc -v
     make distribute

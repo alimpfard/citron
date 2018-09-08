2cab3c9fe4692b4789e23a0aa41b3c5e4291d168
Ali Mohammad Pur <ali.mpfard@gmail.com>
2018-09-09 00:34:44 +0430

    Maybe add file assoc [untested]

--- misc/install.bat
+++ misc/install.bat
@@ -3,23 +3,23 @@ setlocal enabledelayedexpansion enableextensions
 
 call :execute_citron package\prepare_install.ctr, "%SYSTEMDRIVE%\Program Files", Citron
 robocopy package/prepared "%SYSTEMDRIVE%\Program Files\Citron" /s /e
-set "a=%SYSTEMDRIVE%\Program Files\Citron\citron.bat"
-set "b=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Citron\citron.lnk"
-call :path_from_file result "!a!"
+set "a=%SYSTEMDRIVE%\Program Files\Citron\"
+set "b=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Citron\Citron REPL.lnk"
 call :path_from_file linkf "!b!"
 mkdir "%linkf%"
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%b%" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "%a%" >> CreateShortcut.vbs
-echo oLink.WorkingDirectory = "%result%" >> CreateShortcut.vbs
-echo oLink.Description = "Citron REPL" >> CreateShortcut.vbs
+echo oLink.TargetPath = "%a%citron.bat" >> CreateShortcut.vbs
+echo oLink.WorkingDirectory = "%a%" >> CreateShortcut.vbs
+echo oLink.Description = "Project Citron 0.0.8 REPL" >> CreateShortcut.vbs
 echo oLink.IconLocation = "%SYSTEMDRIVE%\Program Files\Citron\image\citron.ico" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 type CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
-
+assoc .ctr=citronfile
+Ftype citronfile="%a%ctr.bat" %%1
 goto eof
 
 :execute_citron 

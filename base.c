5d403e352a43bad3a000f53f62861170e876f0b5
Ali Mohammad Pur Fard <ali.mpfard@gmail.com>
2019-03-08 00:46:16 +0330

    Add extremely crude compiled regular expression replace jobs

--- base.c
+++ base.c
@@ -4021,7 +4021,67 @@ ctr_string_reg_replace (ctr_object * myself, ctr_argument * argumentList)
   CtrStdFlow = ctr_build_string_from_cstring ("Regex replace not implemented for POSIX; use String#findPattern:do:options:");
   return myself;
 }
+ctr_object *
+ctr_string_reg_compile (ctr_object * myself, ctr_argument * argumentList)
+{
+  CtrStdFlow = ctr_build_string_from_cstring ("Regex compile not implemented for POSIX; use String#findPattern:do:options:");
+  return myself;
+}
 #else
+ctr_object *
+ctr_string_reg_compile (ctr_object * myself, ctr_argument * argumentList)
+{
+  if (argumentList->next->object)
+    {				//pattern, substitute, [options]
+      ctr_object *pat = argumentList->object;
+      ctr_object *sub = argumentList->next->object;
+      ctr_object *opts = (argumentList->next->next
+			  && argumentList->next->next->object) ? argumentList->next->next->object : ctr_build_empty_string ();
+      char *p = ctr_heap_allocate_cstring (pat), *s = ctr_heap_allocate_cstring (sub), *o = ctr_heap_allocate_cstring (opts);
+      int err;
+      pcrs_job *job = pcrs_compile (p, s, o, &err);
+      if (job == NULL)
+	{
+	  ctr_heap_free (p);
+	  ctr_heap_free (o);
+	  ctr_heap_free (s);
+	  CtrStdFlow = ctr_build_string_from_cstring ((char *) pcrs_strerror (err));
+	  return CtrStdNil;
+	}
+      ctr_object* res = ctr_object_make(CtrStdObject, NULL);
+      ctr_resource *resource = ctr_heap_allocate(sizeof(*resource));
+      resource->type = 73;
+      resource->ptr = job;
+      res->info.type = CTR_OBJECT_TYPE_OTEX;
+      res->value.rvalue = resource;
+
+      ctr_heap_free (p);
+      ctr_heap_free (o);
+      ctr_heap_free (s);
+      return res;
+    }
+  else
+    {				// s/pattern/replacement/flags
+      ctr_object *cmd = argumentList->object;
+      char *c = ctr_heap_allocate_cstring (cmd);
+      int err;
+      pcrs_job *job = pcrs_compile_command (c, &err);
+      if (job == NULL)
+	{
+	  ctr_heap_free (cmd);
+	  CtrStdFlow = ctr_build_string_from_cstring ((char *) pcrs_strerror (err));
+	  return CtrStdNil;
+	}
+      ctr_object* res = ctr_object_make(CtrStdObject, NULL);
+      ctr_resource *resource = ctr_heap_allocate(sizeof(*resource));
+      resource->type = 73;
+      resource->ptr = job;
+      res->info.type = CTR_OBJECT_TYPE_OTEX;
+      res->value.rvalue = resource;
+      ctr_heap_free (c);
+      return res;
+    }
+}
 ctr_object *
 ctr_string_reg_replace (ctr_object * myself, ctr_argument * argumentList)
 {
@@ -4063,21 +4123,32 @@ ctr_string_reg_replace (ctr_object * myself, ctr_argument * argumentList)
   else
     {				// s/pattern/replacement/flags
       ctr_object *cmd = argumentList->object;
-      char *c = ctr_heap_allocate_cstring (cmd);
+      pcrs_job *job;
       int err;
-      pcrs_job *job = pcrs_compile_command (c, &err);
-      if (job == NULL)
-	{
-	  ctr_heap_free (cmd);
-	  CtrStdFlow = ctr_build_string_from_cstring ((char *) pcrs_strerror (err));
-	  return CtrStdNil;
-	}
+      int alloc = 0;
+      char *c;
+      if (cmd->info.type == CTR_OBJECT_TYPE_OTEX) {
+          job = cmd->value.rvalue->ptr;
+      } else {
+        alloc = 1;
+        c = ctr_heap_allocate_cstring (cmd);
+        job = pcrs_compile_command (c, &err);
+        if (job == NULL)
+  	{
+  	  ctr_heap_free (cmd);
+  	  CtrStdFlow = ctr_build_string_from_cstring ((char *) pcrs_strerror (err));
+  	  return CtrStdNil;
+  	}
+      }
+
       char *input = ctr_heap_allocate_cstring (myself), *result;
       ctr_size length = myself->value.svalue->vlen, rlen;
       if (0 > (err = pcrs_execute (job, input, length, &result, &rlen)))
 	{
-	  pcrs_free_job (job);
-	  ctr_heap_free (c);
+    if (alloc)
+	   pcrs_free_job (job);
+    if (alloc)
+	   ctr_heap_free (c);
 	  ctr_heap_free (input);
 	  CtrStdFlow = ctr_build_string_from_cstring ((char *) pcrs_strerror (err));
 	  return CtrStdNil;

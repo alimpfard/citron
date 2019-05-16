63db065e836315ed6fb50ee167ba6a53fa2edabb
Ali Mohammad Pur <ali.mpfard@gmail.com>
2019-05-15 22:55:47 -0700

    getline

--- system.c
+++ system.c
@@ -1097,7 +1097,7 @@ ctr_object *ctr_command_chdir(ctr_object *myself, ctr_argument *argumentList) {
   char *path = ctr_heap_allocate_cstring(argumentList->object);
 #ifdef DWIN32
   char curpath[PATH_MAX + 1];
-  if (!_fullpath(".", curpath)) {
+  if (!_fullpath(".", curpath, PATH_MAX)) {
 #else
   char *curpath = realpath(".", NULL);
   if (!curpath) {
@@ -1119,6 +1119,59 @@ ctr_object *ctr_command_chdir(ctr_object *myself, ctr_argument *argumentList) {
   CtrStdFlow = err;
   return CtrStdNil;
 }
+#ifdef DWIN32
+size_t getline(char **lineptr, size_t *n, FILE *stream) {
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL) {
+        return -1;
+    }
+    if (stream == NULL) {
+        return -1;
+    }
+    if (n == NULL) {
+        return -1;
+    }
+    bufptr = *lineptr;
+    size = *n;
+
+    c = fgetc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+    if (bufptr == NULL) {
+        bufptr = malloc(128);
+        if (bufptr == NULL) {
+            return -1;
+        }
+        size = 128;
+    }
+    p = bufptr;
+    while(c != EOF) {
+        if ((p - bufptr) > (size - 1)) {
+            size = size + 128;
+            bufptr = realloc(bufptr, size);
+            if (bufptr == NULL) {
+                return -1;
+            }
+        }
+        *p++ = c;
+        if (c == '\n') {
+            break;
+        }
+        c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+#endif
 
 /**
  * [Program] waitForInput

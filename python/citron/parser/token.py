39905856dcf68f91595baa942164a5cb181bc317
Yuval <yuval.tasher@gmail.com>
2021-08-02 14:43:46 +0300

    Move

--- python/citron/parser/token.py
+++
@@ -1,6 +0,0 @@
-from ast import ASTToken
-class CtrToken(ASTToken):
-    """docstring for CtrToken."""
-    def __init__(self, type, value = ""):
-        super().__init__(type)
-        self.value = value
